295d9bd3781c639a39cd0dba0a7b3ccd6f8e6273
Sebastian Krepela <sebous@gmail.com>
2023-12-04 15:57:42 +0100

    day 4

---
+++ src/bin/04.rs
@@ -0,0 +1,68 @@
+use std::collections::{HashMap, HashSet};
+
+use itertools::Itertools;
+
+advent_of_code::solution!(4);
+
+type Card = (HashSet<u32>, HashSet<u32>);
+
+fn parse_input(input: &str) -> Vec<Card> {
+    input
+        .lines()
+        .map(|line| {
+            let numbers: Card = line
+                .split(": ")
+                .nth(1)
+                .unwrap()
+                .split(" | ")
+                .map(|nums| {
+                    nums.split_whitespace()
+                        .map(|n| n.parse::<u32>().unwrap())
+                        .collect::<HashSet<u32>>()
+                })
+                .collect_tuple()
+                .unwrap();
+
+            numbers
+        })
+        .collect_vec()
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let cards = parse_input(input);
+
+    let total = cards.iter().fold(0, |total, (winning, actual)| {
+        let count = (0..winning.intersection(actual).count()).fold(0, |card_total, i| match i {
+            0 => 1,
+            _ => card_total * 2,
+        });
+        total + count
+    });
+
+    Some(total)
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    let mut card_multipliers = HashMap::new();
+    let mut total_card_cnt = 0u64;
+
+    let cards = parse_input(input);
+
+    for (i, (winning, actual)) in cards.iter().enumerate() {
+        let card_no = i as u32 + 1;
+        let points_cnt = winning.intersection(actual).count() as u32;
+
+        let card_mult = card_multipliers.get(&card_no).unwrap_or(&1).clone();
+
+        (card_no + 1..=points_cnt + card_no).for_each(|number| {
+            card_multipliers
+                .entry(number)
+                .and_modify(|mult| *mult += card_mult)
+                .or_insert(1 + card_mult);
+        });
+
+        total_card_cnt += card_mult;
+    }
+
+    Some(total_card_cnt)
+}
